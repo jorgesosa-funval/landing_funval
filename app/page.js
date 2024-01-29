19927e34320472881b270ca96670b7322dd3430d
unknown <jorgesosa@fundaciondevalores.org>
2024-01-29 07:41:03 -0400

    contacts in proccess

--- app/page.js
+++ app/page.js
@@ -16,16 +16,16 @@ export default function Home() {
       <Graduates />
       
       <section
-        class="flex items-center justify-between py-24 courses-section relative px-20"
+        class="flex items-center  justify-center md:justify-between py-24 courses-section relative px-20 overflow-hidden"
       >
-        <div>
+        <div className="flex justify-center w-1/2 flex-shrink-0">
           <form class="w-96 border rounded-md p-8 flex flex-col gap-6 bg-white">
             <h2 class="font-semibold text-xl text-center py-4">CONTACTANOS</h2>
 
             <div>
               <label for="name" class="text-xs">Nombre:</label>
-              <div class="flex shadow-lg h-9 rounded-md">
-                <img src="../public/icons/email.svg" alt="" width="40" height="40" />
+              <div class="flex shadow-lg h-9 rounded-md items-center gap-2 px-3">
+                <img src="/assets/icons/user.svg" alt="" width="40" height="40" className="w-6 h-6"/>
                 <input
                   type="text"
                   name="name"
@@ -38,8 +38,8 @@ export default function Home() {
 
             <div>
               <label for="email" class="text-xs">Correo:</label>
-              <div class="flex shadow-lg h-9 rounded-md">
-                <img src="../public/icons/email.svg" alt="" width="40" height="40" />
+              <div class="flex shadow-lg h-9 rounded-md items-center gap-2 px-3">
+                <img src="/assets/icons/mail.svg" alt="" width="40" height="40" className="w-6 h-6"/>
                 <input
                   type="email"
                   name="email"
@@ -52,8 +52,8 @@ export default function Home() {
 
             <div>
               <label for="phone" class="text-xs">Teléfono:</label>
-              <div class="flex shadow-lg h-9 rounded-md">
-                <img src="../public/icons/email.svg" alt="" width="40" height="40" />
+              <div class="flex shadow-lg h-9 rounded-md items-center gap-2 px-3">
+                <img src="/assets/icons/phone.svg" alt="" width="30" height="40" className="w-6 h-6"/>
                 <input
                   type="tel"
                   name="phone"
@@ -75,6 +75,10 @@ export default function Home() {
 
             <button type="button" class="px-4 py-2 bg-blue-400">Enviar</button>
           </form>
+        </div>
+
+        <div className="hidden w-1/2 md:flex items-center h-full justify-center relative after:bg-slate-900 after:absolute after:w-[540px] after:h-[540px] after:rounded-2xl after:-rotate-45 after:-right-1/2 after:opacity-55 after:-z-10 after:bg-gradient-to-r after:from-green-vogue-400 after:to-green-vogue-100">
+                <img src="/assets/fondos/screen.svg" alt="screen icon" className="w-4/5 -rotate-[30deg]"/>
         </div>
       </section>
       <section class="flex w-full h-24 bg-green-vogue-700">
